22681f02f1225861fcf9fa03e0a3848494510440
wkm <wmacura@gmail.com>
2023-11-05 13:24:39 -0500

    p9 and p10

--- 02-exercises/09-list_range/problem.ml
+++ 02-exercises/09-list_range/problem.ml
@@ -13,7 +13,11 @@ let () =
    including [from] but excluding [to_] in increasing order.
 
    {| val range : int -> int -> int list |} *)
-let range from to_ = failwith "For you to implement"
+let rec range from to_ =
+   if from < to_ then
+      from :: (range (from+1) to_)
+   else
+      []
 
 (* You might've noticed that the list type in the function definitions of the
    operator [( @ )] (and also [( :: )]) look a bit different from every other
