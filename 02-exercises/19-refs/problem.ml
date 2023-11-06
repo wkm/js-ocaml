e26e9eb4ac5a5503acbbafd019648ab9fb6cc0a4
wkm <wmacura@gmail.com>
2023-11-05 19:32:51 -0500

    p19

--- 02-exercises/19-refs/problem.ml
+++ 02-exercises/19-refs/problem.ml
@@ -18,7 +18,12 @@ let () =
    let's iterate over the list and explicitly maintain refs of the minimum and
    maximum values seen so far instead. *)
 let min_and_max lst =
-  failwith "For you to implement"
+  let min = ref Int.max_value in
+  let max = ref Int.min_value in
+  List.iter lst ~f:(function v ->
+    if v < !min then min := v;
+    if !max < v then max := v);
+  (!min, !max)
 
 (* By the way, can you guess how a [ref] is implemented under the hood? 
 
