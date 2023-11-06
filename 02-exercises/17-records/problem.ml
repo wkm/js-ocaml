a309a6767ccc5ad497a3fa52a9ad3cd7298868aa
wkm <wmacura@gmail.com>
2023-11-05 19:21:25 -0500

    p17

--- 02-exercises/17-records/problem.ml
+++ 02-exercises/17-records/problem.ml
@@ -59,8 +59,10 @@ let () = assert (23 = (add_one_to_age an_example).age)
      age set to 30.
 
    - Otherwise, you should increase the number of cars by 6.  *)
-let modify_person (person : person) =
-  failwith "For you to implement"
+let modify_person person =
+  if (String.equal person.first_name "Jan")
+    then {person with age = 30}
+else {person with number_of_cars = person.number_of_cars + 6}
 
 module For_testing = struct
   let test_ex1 : person = {
